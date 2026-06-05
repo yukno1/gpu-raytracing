fd11aef4de6ae53145ad1308664ad136864d15e7
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-06-05 14:55:53 +0800

    pipeline object

--- src/render.rs
+++ src/render.rs
@@ -1,8 +1,9 @@
-use std::sync::Arc;
+use std::{num::NonZero, sync::Arc};
 
 pub struct PathTracer {
     device: wgpu::Device,
     queue: wgpu::Queue,
+    pipeline: wgpu::RenderPipeline,
 }
 
 impl PathTracer {
@@ -11,8 +12,59 @@ impl PathTracer {
             panic!("Aborting due to an error: {}", error);
         }));
 
-        // TODO: initialize GPU resources
+        let shader_module = compile_shader_module(&device);
+        let pipeline = create_pipeline(&device, &shader_module);
 
-        PathTracer { device, queue }
+        PathTracer {
+            device,
+            queue,
+            pipeline,
+        }
     }
+}
+
+fn compile_shader_module(device: &wgpu::Device) -> wgpu::ShaderModule {
+    use std::borrow::Cow;
+
+    let code = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), r"\src\shaders.wgsl"));
+    device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(code)),
+    })
+}
+
+fn create_pipeline(
+    device: &wgpu::Device,
+    shader_module: &wgpu::ShaderModule,
+) -> wgpu::RenderPipeline {
+    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: Some("path tracer"),
+        layout: None,
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            front_face: wgpu::FrontFace::Ccw,
+            polygon_mode: wgpu::PolygonMode::Fill,
+            ..Default::default()
+        },
+        vertex: wgpu::VertexState {
+            module: shader_module,
+            entry_point: Some("path_tracer_vs"),
+            buffers: &[],
+            compilation_options: wgpu::PipelineCompilationOptions::default(),
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: shader_module,
+            entry_point: Some("path_tracer_fs"),
+            targets: &[Some(wgpu::ColorTargetState {
+                format: wgpu::TextureFormat::Bgra8Unorm,
+                blend: None,
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+            compilation_options: wgpu::PipelineCompilationOptions::default(),
+        }),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        multiview_mask: NonZero::new(0 as u32),
+        cache: None,
+    })
 }
